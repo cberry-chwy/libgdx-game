46190ac04004189f56e1b09bf26a58f21e90be3b
Craig Berry <cberry.swe@gmail.com>
2020-04-11 16:59:55 -0400

    bring in tools libs, generate fonts, reorg assets

--- core/src/dev/cberry/gdxgame/MyGdxGame.kt
+++ core/src/dev/cberry/gdxgame/MyGdxGame.kt
@@ -6,18 +6,24 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.TextField
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 
+
+/**
+ * Todo
+ * 1. Text on title screen
+ * 2. Transition to new screen after delay
+ * 3. Draw grid
+ * 4. Make something move
+ */
 class MyGdxGame : ApplicationAdapter() {
 
     private val badLogicTexture: Texture by lazy {
-        Texture("badlogic.jpg")
+        Texture("images/badlogic.jpg")
     }
 
     private val hollyTexture: Texture by lazy {
-        Texture("holler.jpg")
+        Texture("images/holler.jpg")
     }
 
     private val stage: Stage by lazy {
